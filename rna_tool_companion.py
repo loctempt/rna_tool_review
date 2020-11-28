f6e4bfb79fc20849c49a521318e62217daa21f59
llleichuqi <291573319@qq.com>
2020-11-28 22:59:22 +0800

    添加了调用UCSF Chimera实现蛋白质叠合的功能，将口袋计算、柔性氨基酸鉴定的代码注释了

--- rna_tool_companion.py
+++ rna_tool_companion.py
@@ -1,5 +1,7 @@
 from rna_tool_new import *
 import os
+import shutil
+import subprocess
 import math
 
 
@@ -20,7 +22,6 @@ def dist_cutoff_cal(micro_atom_list, macro_atom_list, cutoff):
             break
     return atom_cnt
 
-
 def input_legalization(marked_words, legal_num, case_status):
     flag = True
     while flag is True:
@@ -28,6 +29,27 @@ def input_legalization(marked_words, legal_num, case_status):
         if len(tmp) == legal_num and case_status(tmp):
             flag = False
     return tmp
+
+def call_perl_and_rename(rename_to,matchPath,script_path):
+    print('start running perl script...')
+    os.chdir(matchPath)
+    print('call_perl:',os.getcwd())
+    ret = subprocess.call(['perl', script_path+'/superimopose.pl'])
+    if ret == 0:
+            os.rename(matchPath+'/align_tmp.pdb', matchPath+'/'+rename_to)
+            print('success')
+    else:
+        print('failed')
+
+def call_superimopose(pdb_id,data_pth):
+    pdb_list=os.listdir(data_pth+'/pre_dealing')
+    shutil.copyfile(data_pth+'/pre_dealing/'+pdb_id+'.pdb',data_pth+'/superimpose/template.pdb')
+    for i in pdb_list:
+        shutil.copyfile(data_pth+'/pdb/'+i,data_pth+'/superimpose/align.pdb')
+        call_perl_and_rename(i,data_pth+'/superimpose',script_pth)
+        # TODO script path undefined
+        os.remove(data_pth+'/superimpose/align.pdb') 
+    os.remove(data_pth+'/superimpose/template.pdb')
 
 
 dirPath = input("输入当前这一类蛋白（如激酶）的目录:")
@@ -172,8 +194,9 @@ for dir in dirList:
             if len(value) > 1:
                 outFile.writelines(str(micro_chain))
 
-    # 叠合
-    print('下一步：蛋白叠合，请移至服务器完成')
+    # superimpose
+    # print('下一步：蛋白叠合，请移至服务器完成')
+    call_superimopose(templateID,dirPath+'/'+dir)
     os.system('pause')
 
     # =========================
@@ -182,144 +205,144 @@ for dir in dirList:
     # 读文件构造pdb对象
     # 计算与各个小分子距离在5埃内的分子  构造大小分子dict{key,[macro,micro]}
     #  没有小分子的直接输出一个大分子即可 大小分子间加TER
-    superimposefileList = os.listdir(filePath+'\\superimpose\\input')
-    # rough_sele_pr_dict.clear()
-    rough_sele_pr_dict = {}
-    for surfile in superimposefileList:
-        print(surfile)
-        with open(filePath+'\\superimpose\\input\\'+surfile, 'r') as cur_file:
-            cur_PDB = PDB(cur_file.readlines())
-            if cur_PDB.micro_molecule != None:
-                rough_sele_pr_dict[str(surfile[:5])] = [cur_PDB.macro_molecule.get_chain(
-                    surfile[4]), cur_PDB.micro_molecule.get_chain(surfile[4])]
-            else:
-                rough_sele_pr_dict[str(surfile[:5])] = [
-                    cur_PDB.macro_molecule.get_chain(surfile[4])]
+    # superimposefileList = os.listdir(filePath+'\\superimpose\\input')
+    # # rough_sele_pr_dict.clear()
+    # rough_sele_pr_dict = {}
+    # for surfile in superimposefileList:
+    #     print(surfile)
+    #     with open(filePath+'\\superimpose\\input\\'+surfile, 'r') as cur_file:
+    #         cur_PDB = PDB(cur_file.readlines())
+    #         if cur_PDB.micro_molecule != None:
+    #             rough_sele_pr_dict[str(surfile[:5])] = [cur_PDB.macro_molecule.get_chain(
+    #                 surfile[4]), cur_PDB.micro_molecule.get_chain(surfile[4])]
+    #         else:
+    #             rough_sele_pr_dict[str(surfile[:5])] = [
+    #                 cur_PDB.macro_molecule.get_chain(surfile[4])]
 
-    specified_sele_pkt_dict = {}
-    ligand_cnt = 0
-    tmp_key = 0
-    for key, value in rough_sele_pr_dict.items():
-        macro_chain = value[0]
-        micro_chain = value[1] if len(value) == 2 else None
-        if len(value) == 1:
-            specified_sele_pkt_dict[key] = [macro_chain]
-        else:
-            # 对每种key单独编号
-            if tmp_key != key:
-                tmp_key = key
-                ligand_cnt = 0
-            for micro_aa in micro_chain.get_complete_aa():
-                tmp_pkt_list = []
-                for macro_aa in macro_chain.get_complete_aa():
-                    dist_cutoff = dist_cutoff_cal(
-                        macro_aa.get_complete_atom(), micro_aa.get_complete_atom(), 5)
-                    if dist_cutoff == 0:
-                        continue
-                    # 记录落入口袋半径的氨基酸
-                    tmp_pkt_list.append(macro_aa)
-                specified_sele_pkt_dict[key+'_' +
-                                        str(ligand_cnt)] = [tmp_pkt_list, micro_aa]
-                ligand_cnt += 1
+    # specified_sele_pkt_dict = {}
+    # ligand_cnt = 0
+    # tmp_key = 0
+    # for key, value in rough_sele_pr_dict.items():
+    #     macro_chain = value[0]
+    #     micro_chain = value[1] if len(value) == 2 else None
+    #     if len(value) == 1:
+    #         specified_sele_pkt_dict[key] = [macro_chain]
+    #     else:
+    #         # 对每种key单独编号
+    #         if tmp_key != key:
+    #             tmp_key = key
+    #             ligand_cnt = 0
+    #         for micro_aa in micro_chain.get_complete_aa():
+    #             tmp_pkt_list = []
+    #             for macro_aa in macro_chain.get_complete_aa():
+    #                 dist_cutoff = dist_cutoff_cal(
+    #                     macro_aa.get_complete_atom(), micro_aa.get_complete_atom(), 5)
+    #                 if dist_cutoff == 0:
+    #                     continue
+    #                 # 记录落入口袋半径的氨基酸
+    #                 tmp_pkt_list.append(macro_aa)
+    #             specified_sele_pkt_dict[key+'_' +
+    #                                     str(ligand_cnt)] = [tmp_pkt_list, micro_aa]
+    #             ligand_cnt += 1
 
-    for key, value in specified_sele_pkt_dict.items():
-        macro_chain = value[0]
-        micro_chain = value[1] if len(value) == 2 else None
-        print(key)
-        with open(filePath+'\\superimpose\\output\\'+key+'.pdb', 'w')as outFile:
-            if len(value) == 1:
-                outFile.writelines(str(macro_chain))
-            else:
-                for i in macro_chain:
-                    outFile.writelines(str(i))
-                    outFile.write('\n')
-                outFile.write('TER\n')
-                outFile.write(str(micro_chain))
+    # for key, value in specified_sele_pkt_dict.items():
+    #     macro_chain = value[0]
+    #     micro_chain = value[1] if len(value) == 2 else None
+    #     print(key)
+    #     with open(filePath+'\\superimpose\\output\\'+key+'.pdb', 'w')as outFile:
+    #         if len(value) == 1:
+    #             outFile.writelines(str(macro_chain))
+    #         else:
+    #             for i in macro_chain:
+    #                 outFile.writelines(str(i))
+    #                 outFile.write('\n')
+    #             outFile.write('TER\n')
+    #             outFile.write(str(micro_chain))
 
     # pymol 删除不可用的口袋 (手动处理后放至文件夹：flex_site-> input)
-    print('请将候选口袋初步判断后，将候选口袋移至flex_site/input文件夹，进行下一步')
-    os.system('pause')
-    # =========================
-    # 鉴定柔性
-    # ========================
-    # 读文件构造pdb对象
-    # set()存各个现存的氨基酸编号  遍历上一步的dict中取通用口袋
-    # 计算rmsd值 每一个氨基酸区分和不区分 sidechain 和 backbone分别对原子计算
-    # 找到sidechain和backbone的最大值 实际上要找到每个氨基酸的最大rmsd值 保存该口袋
-    # 格式：2OF2A
-    rough_template_pkt = input_legalization(
-        "DUD-E" + dir + " receptor(2OF2A)：", 5, str.isupper)
-    rough_pkfileList = os.listdir(filePath+'\\flex_site\\input')
-    specified_sele_pkt_dict = {}
-    # specified_sele_pkt_dict.clear()
-    for rgh_file in rough_pkfileList:
-        with open(filePath+'\\flex_site\\input\\'+rgh_file, 'r') as cur_file:
-            print(rgh_file)
-            cur_PDB = PDB(cur_file.readlines())
-            if cur_PDB.micro_molecule != None:
-                print(rgh_file[4])
-                # print(str(cur_PDB.macro_molecule.get_chain(rgh_file[4])))
-                # if not specified_sele_pkt_dict.get(str(rgh_file[:5])):
-                #     specified_sele_pkt_dict[str(rgh_file[:5])]=cur_PDB.macro_molecule.get_chain(rgh_file[4])
-                # else:
-                specified_sele_pkt_dict[str(
-                    rgh_file[:5])] = cur_PDB.macro_molecule.get_chain(rgh_file[4])
+    # print('请将候选口袋初步判断后，将候选口袋移至flex_site/input文件夹，进行下一步')
+    # os.system('pause')
+    # # =========================
+    # # 鉴定柔性
+    # # ========================
+    # # 读文件构造pdb对象
+    # # set()存各个现存的氨基酸编号  遍历上一步的dict中取通用口袋
+    # # 计算rmsd值 每一个氨基酸区分和不区分 sidechain 和 backbone分别对原子计算
+    # # 找到sidechain和backbone的最大值 实际上要找到每个氨基酸的最大rmsd值 保存该口袋
+    # # 格式：2OF2A
+    # rough_template_pkt = input_legalization(
+    #     "DUD-E" + dir + " receptor(2OF2A)：", 5, str.isupper)
+    # rough_pkfileList = os.listdir(filePath+'\\flex_site\\input')
+    # specified_sele_pkt_dict = {}
+    # # specified_sele_pkt_dict.clear()
+    # for rgh_file in rough_pkfileList:
+    #     with open(filePath+'\\flex_site\\input\\'+rgh_file, 'r') as cur_file:
+    #         print(rgh_file)
+    #         cur_PDB = PDB(cur_file.readlines())
+    #         if cur_PDB.micro_molecule != None:
+    #             print(rgh_file[4])
+    #             # print(str(cur_PDB.macro_molecule.get_chain(rgh_file[4])))
+    #             # if not specified_sele_pkt_dict.get(str(rgh_file[:5])):
+    #             #     specified_sele_pkt_dict[str(rgh_file[:5])]=cur_PDB.macro_molecule.get_chain(rgh_file[4])
+    #             # else:
+    #             specified_sele_pkt_dict[str(
+    #                 rgh_file[:5])] = cur_PDB.macro_molecule.get_chain(rgh_file[4])
                     
-    pkt_resSeq = set()
-    for key, value in specified_sele_pkt_dict.items():
-        for macro_aa in value.get_complete_aa():
-            pkt_resSeq.add(macro_aa.atom_list[0].get_resSeq())
+    # pkt_resSeq = set()
+    # for key, value in specified_sele_pkt_dict.items():
+    #     for macro_aa in value.get_complete_aa():
+    #         pkt_resSeq.add(macro_aa.atom_list[0].get_resSeq())
 
-    pkt_resSeq = sorted(list(pkt_resSeq))
+    # pkt_resSeq = sorted(list(pkt_resSeq))
 
-    specified_sele_pkt_dict.clear()
-    for key, value in rough_sele_pr_dict.items():
-        tmp_sp_pkt_list = []
-        for macro_aa in value[0].get_complete_aa():
-            if macro_aa.atom_list[0].get_resSeq() in pkt_resSeq:
-                tmp_sp_pkt_list.append(macro_aa)
-        specified_sele_pkt_dict[key] = [tmp_sp_pkt_list]
+    # specified_sele_pkt_dict.clear()
+    # for key, value in rough_sele_pr_dict.items():
+    #     tmp_sp_pkt_list = []
+    #     for macro_aa in value[0].get_complete_aa():
+    #         if macro_aa.atom_list[0].get_resSeq() in pkt_resSeq:
+    #             tmp_sp_pkt_list.append(macro_aa)
+    #     specified_sele_pkt_dict[key] = [tmp_sp_pkt_list]
 
-    template_pkt = specified_sele_pkt_dict[rough_template_pkt][0]
-    max_rmsd_pkt = dict(map(lambda item: (item, float(0)), pkt_resSeq))
-    max_pkt = dict(map(lambda item: (item, None), pkt_resSeq))
-    for key, value in specified_sele_pkt_dict.items():
-        print(key, 'len:', len(value[0]))
-        for cur_aa_no in range(len(value[0])):
-            cur_resSeq = value[0][cur_aa_no].get_molecule_resSeq()
-            cur_resName = value[0][cur_aa_no].get_molecule_resName()
-            cur_aa_len = value[0][cur_aa_no].get_molecule_len()
-            for temp_aa in template_pkt:
-                if temp_aa.get_molecule_resSeq() != cur_resSeq or temp_aa.get_molecule_resName() != cur_resName or len(temp_aa.atom_list) != cur_aa_len:
-                    continue
-                rmsd, atom_list, sc_list, bb_list = value[0][cur_aa_no].rmsd_cal(
-                    temp_aa)
+    # template_pkt = specified_sele_pkt_dict[rough_template_pkt][0]
+    # max_rmsd_pkt = dict(map(lambda item: (item, float(0)), pkt_resSeq))
+    # max_pkt = dict(map(lambda item: (item, None), pkt_resSeq))
+    # for key, value in specified_sele_pkt_dict.items():
+    #     print(key, 'len:', len(value[0]))
+    #     for cur_aa_no in range(len(value[0])):
+    #         cur_resSeq = value[0][cur_aa_no].get_molecule_resSeq()
+    #         cur_resName = value[0][cur_aa_no].get_molecule_resName()
+    #         cur_aa_len = value[0][cur_aa_no].get_molecule_len()
+    #         for temp_aa in template_pkt:
+    #             if temp_aa.get_molecule_resSeq() != cur_resSeq or temp_aa.get_molecule_resName() != cur_resName or len(temp_aa.atom_list) != cur_aa_len:
+    #                 continue
+    #             rmsd, atom_list, sc_list, bb_list = value[0][cur_aa_no].rmsd_cal(
+    #                 temp_aa)
 
-                if rmsd >= max_rmsd_pkt[value[0][cur_aa_no].atom_list[0].get_resSeq()]:
-                    max_rmsd_pkt[cur_resSeq] = rmsd
-                    max_pkt[cur_resSeq] = temp_aa
-    # 删除max_pkt中为None的aa记录
-    for key in list(max_pkt.keys()):
-        if max_pkt[key] is None:
-            del max_pkt[key]
-            del max_rmsd_pkt[key]
+    #             if rmsd >= max_rmsd_pkt[value[0][cur_aa_no].atom_list[0].get_resSeq()]:
+    #                 max_rmsd_pkt[cur_resSeq] = rmsd
+    #                 max_pkt[cur_resSeq] = temp_aa
+    # # 删除max_pkt中为None的aa记录
+    # for key in list(max_pkt.keys()):
+    #     if max_pkt[key] is None:
+    #         del max_pkt[key]
+    #         del max_rmsd_pkt[key]
 
-    # 添加计算中心点距离的功能
+    # # 添加计算中心点距离的功能
 
-    # 添加rmsd值排序以及标注氨基酸的功能
-    sorted_max_rmsd_pkt = sorted(
-        max_rmsd_pkt.items(), key=lambda item: item[1], reverse=True)
+    # # 添加rmsd值排序以及标注氨基酸的功能
+    # sorted_max_rmsd_pkt = sorted(
+    #     max_rmsd_pkt.items(), key=lambda item: item[1], reverse=True)
 
-    with open(filePath+'\\flex_site\\output\\'+dir+'_'+rough_template_pkt+'_pocket.pdb', 'w') as pocket, open(filePath+'\\flex_site\\output\\'+rough_template_pkt+'_pocket_rmsd.txt', 'w') as pocket_rmsd:
-        # for i in range(len,ax)
-        for pkt_key in max_pkt:
-            pocket.writelines(str(max_pkt[pkt_key]))
-            pocket.write('\n')
-        for rmsd_key in sorted_max_rmsd_pkt:
-            pocket_rmsd.write(str(rmsd_key[0])+'  '+str(rmsd_key[1]))
-            # print(max_pkt[rmsd_key[0]].get_molecule_resName())
-            if max_pkt[rmsd_key[0]].get_molecule_resName() in ['ALA', 'GLY', 'PRO']:
-                pocket_rmsd.write(
-                    '  '+max_pkt[rmsd_key[0]].get_molecule_resName())
-            pocket_rmsd.write('\n')
-            # print(max_rmsd_pkt[key],'\n',max_pkt[key])
+    # with open(filePath+'\\flex_site\\output\\'+dir+'_'+rough_template_pkt+'_pocket.pdb', 'w') as pocket, open(filePath+'\\flex_site\\output\\'+rough_template_pkt+'_pocket_rmsd.txt', 'w') as pocket_rmsd:
+    #     # for i in range(len,ax)
+    #     for pkt_key in max_pkt:
+    #         pocket.writelines(str(max_pkt[pkt_key]))
+    #         pocket.write('\n')
+    #     for rmsd_key in sorted_max_rmsd_pkt:
+    #         pocket_rmsd.write(str(rmsd_key[0])+'  '+str(rmsd_key[1]))
+    #         # print(max_pkt[rmsd_key[0]].get_molecule_resName())
+    #         if max_pkt[rmsd_key[0]].get_molecule_resName() in ['ALA', 'GLY', 'PRO']:
+    #             pocket_rmsd.write(
+    #                 '  '+max_pkt[rmsd_key[0]].get_molecule_resName())
+    #         pocket_rmsd.write('\n')
+    #         # print(max_rmsd_pkt[key],'\n',max_pkt[key])
